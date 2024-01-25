e693c2d68d6f093de16167e387eb4ead67bc6253
Mario Chico <chico.developer@hotmail.com>
2024-01-25 17:37:51 -0600

    Agregando Svelte al proyecto

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -51,10 +51,7 @@
 						/>
 					</svg>
 				</div>
-
-				<p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
-					Copyright &copy; 2022. All rights reserved.
-				</p>
+				<slot />
 			</div>
 		</div>
 	</footer>
---
+++ src/components/Social.svelte
@@ -0,0 +1,3 @@
+<p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+  Copyright &copy; 2022. All rights reserved.
+</p>
